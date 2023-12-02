d763457f0c3936a9d17b10092fbd7cb5d6854d2d
KnightNight101 <nithings@tuta.io>
2023-12-02 16:33:44 +0000

    Hasn't really worked it just prints the class instead of the date and time

---
+++ Python_Projects/Basic_Project/Current Date and Time.py
@@ -0,0 +1,10 @@
+"""
+Write a Python program to display the current date and time.
+Sample Output :
+Current date and time :
+2014-07-05 14:34:14
+"""
+import datetime
+
+
+print("The Date is: " + str(date.today()) + "\nThe time is: " + str(datetime.time))
