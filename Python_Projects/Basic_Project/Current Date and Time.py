d0a64162ef3a2703794a2e6f8359f197f0db1f1e
KnightNight101 <nithings@tuta.io>
2023-12-02 21:02:27 +0000

    Broke down the problem to get date working

--- Python_Projects/Basic_Project/Current Date and Time.py
+++ Python_Projects/Basic_Project/Current Date and Time.py
@@ -4,7 +4,10 @@ Sample Output :
 Current date and time :
 2014-07-05 14:34:14
 """
-import datetime
+from datetime import date 
 
-
-print("The Date is: " + str(date.today()) + "\nThe time is: " + str(datetime.time))
+current_date = date.today()
+#current_time = time.time()
+# For debugging purposes
+print(current_date)
+#print(time)
