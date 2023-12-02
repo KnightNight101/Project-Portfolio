da72919e483a19e4bc1ea78557a6b62d529294ee
KnightNight101 <nithings@tuta.io>
2023-12-02 15:54:52 +0000

    Creating directories for Project Portfolio

---
+++ Python_Projects/Basic Project/Specific Formats of Text Output.py
@@ -0,0 +1,17 @@
+"""Write a Python program to print the following string in a specific format (see the output).
+Sample String: 
+Twinkle, twinkle, little star, 
+    How I wonder what you are! 
+        Up above the world so high, 
+        Like a diamond in the sky. 
+    Twinkle, twinkle, little star, 
+How I wonder what you are
+"""
+line1 = "Twinkle, twinkle, little star,"
+line2 = "   How I wonder what you are!"
+line3 = "       Up above the world so high,"
+line4 = "       Like a diamond in the sky."
+line5 = "   Twinkle, twinkle, little star"
+line6 = "How I wonder what you are"
+
+print(line1+"\n"+line2+"\n"+line3+"\n"+line4+"\n"+line5+"\n"+line6)
