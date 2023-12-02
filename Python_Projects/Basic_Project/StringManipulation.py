d2deb7ce1d118f8326489a30da88948454cc57a5
KnightNight101 <nithings@tuta.io>
2023-12-02 22:27:32 +0000

    Problem 5 Solved!

--- Python_Projects/Basic_Project/StringManipulation.py
+++ Python_Projects/Basic_Project/StringManipulation.py
@@ -4,4 +4,9 @@ Write a Python program that accepts the user's first and last name and prints th
 print("Hello! This program converts a first name and last name and prints them in reverse! \n")
 input = input("Please write your first and last name with a space inbetween!: ")
 reverse_input = input[::-1]
-print(reverse_input)
+print(reverse_input+ "\n")
+sliced_input = input.split()
+first,last = sliced_input
+print("First name: " + first + "\n")
+print("Last name: " + last + "\n")
+print("Reversed: " + last +" "+ first)
