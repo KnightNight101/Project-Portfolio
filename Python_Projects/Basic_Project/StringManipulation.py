cd03a845f6be310e52573c30673296e3ee34dc38
KnightNight101 <nithings@tuta.io>
2023-12-02 22:07:46 +0000

    Input received and printed

---
+++ Python_Projects/Basic_Project/StringManipulation.py
@@ -0,0 +1,7 @@
+"""
+Write a Python program that accepts the user's first and last name and prints them in reverse order with a space between them. 
+"""
+print("Hello! This program converts a first name and last name and prints them in reverse! \n")
+input = input("Please write your first and last name with a space inbetween!: ")
+#for debug purposes
+print(input)
