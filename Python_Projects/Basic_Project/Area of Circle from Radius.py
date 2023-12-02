ead3394b553af1b95c5673224b9617bfd9f19e70
KnightNight101 <nithings@tuta.io>
2023-12-02 21:20:21 +0000

    The required output is reached! Code still needs to be optimised

--- Python_Projects/Basic_Project/Area of Circle from Radius.py
+++ Python_Projects/Basic_Project/Area of Circle from Radius.py
@@ -4,5 +4,9 @@ Sample Output :
 r = 1.1
 Area = 3.8013271108436504
 """
+import math
 UserInput = float(input("Enter a radius in mm to get the area of the circle with that radius: "))
 print(str(UserInput))
+PI = math.pi
+Area = PI*UserInput*UserInput
+print("The Area of the circle is: "+str(Area)+"mm^2")
