307795fff3a41da9cd9397555cf8a2917a7f99c6
KnightNight101 <nithings@tuta.io>
2023-12-02 16:18:32 +0000

    Problem 1 Solved!
    1 of many. Used the sample solution to improve the initial code written

--- Python_Projects/Basic_Project/Specific Formats of Text Output.py
+++ Python_Projects/Basic_Project/Specific Formats of Text Output.py
@@ -7,6 +7,9 @@ Twinkle, twinkle, little star,
     Twinkle, twinkle, little star, 
 How I wonder what you are
 """
+# Trial 1
+
+"""
 line1 = "Twinkle, twinkle, little star,"
 line2 = "   How I wonder what you are!"
 line3 = "       Up above the world so high,"
@@ -15,3 +18,13 @@ line5 = "   Twinkle, twinkle, little star"
 line6 = "How I wonder what you are"
 
 print(line1+"\n"+line2+"\n"+line3+"\n"+line4+"\n"+line5+"\n"+line6)
+"""
+
+# The solution as provided
+# print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
+# Updating my code with the \t command
+
+a, b, c, d = "Twinkle, twinkle, little star," , "How I wonder what you are!","Up above the world so high," ,"Like a diamond in the sky."
+print(a+"\n\t"+b+"\n\t\t"+c+"\n\t\t"+d+"\n\t"+a+"\n"+b)
+
+
