8bd3f8afdf6fb6e9c99fd8c75df799d7578392f0
KnightNight101 <nithings@tuta.io>
2023-12-02 21:16:17 +0000

    User input has been accepted and that's been printed for debug purposes

---
+++ Python_Projects/Basic_Project/Area of Circle from Radius.py
@@ -0,0 +1,8 @@
+"""
+Write a Python program that calculates the area of a circle based on the radius entered by the user.
+Sample Output :
+r = 1.1
+Area = 3.8013271108436504
+"""
+UserInput = float(input("Enter a radius in mm to get the area of the circle with that radius: "))
+print(str(UserInput))
