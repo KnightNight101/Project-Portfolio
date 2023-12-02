4b8fe5021438d4db660a44ac34ca7ae7d8c3c780
KnightNight101 <nithings@tuta.io>
2023-12-02 16:26:33 +0000

    Completed Problem 2, created code to check Python Version and System Information

---
+++ Python_Projects/Basic_Project/Version of Python.py
@@ -0,0 +1,9 @@
+"""
+Write a Python program to find out what version of Python you are using.
+"""
+
+import sys
+
+print("Python version:" + str(sys.version))
+print("System Version Information: "+ str(sys.version_info))
+
