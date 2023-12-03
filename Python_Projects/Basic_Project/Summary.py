4787d8ae531b17e43a7d09a66954ad85c073b6e2
KnightNight101 <nithings@tuta.io>
2023-12-03 15:58:46 +0000

    Created a summary project
    In order to use all the techniques learned, a new summary project has been created.
    This is where I can check I have truly learned things and as such will not have any
    ctrl+c,ctrl+v code in it.

---
+++ Python_Projects/Basic_Project/Summary.py
@@ -0,0 +1,15 @@
+# So in order to keep the techniques learned fresh, I think that it's best to create a seperate project that has all of them in it
+# This will NOT have copied code, but will have the topics and commands learned in it
+
+# A place to import things
+import sys
+# Going to start with the version of python 
+print("Hello, welcome to the Learning Summary Project! \nWe're currently Running on version: "+str(sys.version))
+# String Manipulation
+FullName = input("Please enter your first and last name: ")
+FirstName, LastName = FullName.split()
+print("Ah, " + str(LastName)+ " , first name " + str(FirstName) " !\n")
+Mood = input("How are you doing today?: ")
+print("Hmmm.... \n")
+# Pick Object from list
+
