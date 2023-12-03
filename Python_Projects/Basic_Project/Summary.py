876937c7aa2f5d7e67d293e4ee63f76596fcf67f
KnightNight101 <nithings@tuta.io>
2023-12-03 17:53:59 +0000

    Update to the Summary, including 4 of techniques learned through the first 10 problems

--- Python_Projects/Basic_Project/Summary.py
+++ Python_Projects/Basic_Project/Summary.py
@@ -8,10 +8,17 @@ print("Hello, welcome to the Learning Summary Project! \nWe're currently Running
 # String Manipulation
 FullName = input("Please enter your first and last name: ")
 FirstName, LastName = FullName.split()
-print("Ah, " + str(LastName)+ " , first name " + str(FirstName) + " !\n")
+print("Ah yes of course, " + str(LastName)+ ", " + str(FirstName) + "!\n")
 Mood = input("How are you doing today?: ")
 print("Hmmm.... \n")
 # Pick Object from list
 FriendsInput = input("So tell me about your friends, who would you say are your top 3? (separate with commas): ")
 FriendsList = FriendsInput.split(",")
-Useless = input("Ok so here's a fun question, why is "+FriendsList[2]+" at the bottom of the list?: ")
+# Specific Format of Output Text
+BlackmailInput = input("\nOk so here's a fun question, why is "+FriendsList[2]+
+                       " at the bottom of the list? Why are the other two, "+FriendsList[0]+" and "+FriendsList[1]+", higher on your list?: ")
+print("Good to know...\n\nSend to: "+FriendsList[0]+"@gmail.com; "+FriendsList[1]+"@gmail.com; "+FriendsList[2]+"@gmail.com;\n\nDear "+
+      FriendsList[0]+" "+FriendsList[1]+" "+FriendsList[2]+"\n\nThis is an automated email to let everyone know what's going on!\nSo it would appear"+ 
+      "that this is what "+FirstName+" thinks of you:\n\t"+BlackmailInput+"\n\nwhich I just thought was fascinating, don't you?\n\n"+
+      "Kind Regards,\n\nPythonAutomationService")
+YesNo = input("\nReady to send?: ")
