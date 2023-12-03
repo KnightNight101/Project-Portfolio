bc68719186742ec6b53a6f8d86c63dc15404d6ac
KnightNight101 <nithings@tuta.io>
2023-12-03 15:41:52 +0000

    Got it working, room for efficiency

--- Python_Projects/Basic_Project/0010 - Integers manipulation.py
+++ Python_Projects/Basic_Project/0010 - Integers manipulation.py
@@ -3,3 +3,7 @@ Write a Python program that accepts an integer (n) and computes the value of n+n
 Sample value of n is 5
 Expected Result : 615
 """
+# Using user input to keep things interesting
+InputValue = int(input("Type in an integer: "))
+OutputValue = InputValue + (11*InputValue) + (111*InputValue)
+print(OutputValue)
