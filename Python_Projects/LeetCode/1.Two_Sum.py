67dcc938d0c2ab7fa84f2f91ed3e7a405bf806c5
KnightNight101 <nithings@tuta.io>
2023-12-05 18:26:18 +0000

    Update to 1.Two_Sum.py, having found how to do less thans in python

--- Python_Projects/LeetCode/1.Two_Sum.py
+++ Python_Projects/LeetCode/1.Two_Sum.py
@@ -25,4 +25,17 @@ Constraints:
 
 # Before optimising code, I want to do this via brute force. So by this I mean that I want to predetermine what the numbers and the target is/are
 # and then go through every option available
+Nums = [2,7,11,15]
+Target = 9
+print(str(Nums)+"\n"+str(Target))
+# So with that done I now want to find which elements sum to the target integer
+# My first thought is that if it's summing up to the target then it must be less than the target. I think if I find which elements are less than
+# the target then I can find them more easily
+
+# Just Learned about something called List comprehension which sounds like what I am after!
+NumsLTarget = [i for i in Nums if i <= Target]
+print(NumsLTarget)
+
+
+
 
